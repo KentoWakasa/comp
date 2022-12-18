54cea1b23a21f39fdc05e60dd5ccc026a467bda9
KentoWakasa <103986115+KentoWakasa@users.noreply.github.com>
2022-12-18 23:48:17 +0000

    create basic frame

--- app/controllers/public/post_images_controller.rb
+++ app/controllers/public/post_images_controller.rb
@@ -1,23 +1,38 @@
 class Public::PostImagesController < ApplicationController
   def new
-    @post_image = post_image.new
+    @post_image = PostImage.new
   end
 
   def create
-    @post_image = post_image.new
-    @post_image.save
+    post_image = PostImage.new(post_image_params
+    )
+    post_image.save
     redirect_to post_images_index_path
   end
 
   def index
+    @post_image = PostImage.all
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
   end
 
   def edit
+    @post_image = PostImage.find(params[:id])
   end
 
+  def update
+    post_image = PostImage.find(params[:id])
+    post_image.update(post_image_params)
+    redirect_to post_images_index_path(post_image.id)
+  end
+
+  def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to post_images_index_path
+  end
 
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
     get 'post_images/new'
     post 'post_images' => 'post_images#create'
     get 'post_images/index'
-    get 'post_images/show'
-    get 'post_images/edit'
+    get 'post_images/show/:id' => 'post_images#show', as: 'show_post_image'
+    get 'post_images/edit/:id' => 'post_images#edit', as: 'edit_post_image'
+    patch 'post_images/update/:id' => 'post_images#update', as: 'update_post_image'
+    delete 'post_images/destroy/:id' => 'post_images#destroy', as: 'destroy_post_image'
   end
 
   devise_for :admin, skip: [:registrations, :passwords], controller: {
